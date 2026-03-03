71dfd59188349b47f123f817e853249c6fdd5eef
opilane <opilane@HP-CZC2349HTS>
2026-03-03 14:10:29 +0200

    Add project files.

---
+++ KeelteKooli/Controllers/AdminController.cs
@@ -0,0 +1,360 @@
+﻿using KeelteKooli.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Host.SystemWeb;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KeelteKooli.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+
+        private ApplicationUserManager UserManager =>
+            HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+
+        // ---------------- COURSES ----------------
+        public ActionResult Courses()
+        {
+            var courses = db.Courses.ToList();
+            return View("Courses/Index", courses);
+        }
+
+        public ActionResult CreateCourse()
+        {
+            return View("Courses/Create");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateCourse(Course course)
+        {
+            if (!ModelState.IsValid)
+                return View("Courses/Create", course);
+
+            db.Courses.Add(course);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Courses));
+        }
+
+        public ActionResult EditCourse(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var course = db.Courses.Find(id);
+            if (course == null) return HttpNotFound();
+
+            return View("Courses/Edit", course);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditCourse(Course course)
+        {
+            if (!ModelState.IsValid)
+                return View("Courses/Edit", course);
+
+            db.Entry(course).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction(nameof(Courses));
+        }
+
+        public ActionResult DeleteCourse(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var course = db.Courses.Find(id);
+            if (course == null) return HttpNotFound();
+
+            return View("Courses/Delete", course);
+        }
+
+        [HttpPost, ActionName("DeleteCourse")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteCourseConfirmed(int id)
+        {
+            var course = db.Courses.Find(id);
+            if (course == null) return HttpNotFound();
+
+            db.Courses.Remove(course);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Courses));
+        }
+
+        // ---------------- TEACHERS (OPETAJA) ----------------
+        public ActionResult Teachers()
+        {
+            var teachers = db.Teachers.Include(t => t.User).ToList();
+            return View("Opetaja/Index", teachers);
+        }
+
+        public ActionResult CreateTeacher()
+        {
+            return View("Opetaja/Create");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateTeacher(Teacher teacher, string Password)
+        {
+            if (string.IsNullOrWhiteSpace(teacher.Email))
+                ModelState.AddModelError("Email", "Email is required");
+            if (string.IsNullOrWhiteSpace(Password))
+                ModelState.AddModelError("Password", "Password is required");
+
+            if (!ModelState.IsValid)
+                return View("Opetaja/Create", teacher);
+
+            var newUser = new ApplicationUser
+            {
+                UserName = teacher.Email,
+                Email = teacher.Email
+            };
+
+            var result = UserManager.Create(newUser, Password);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", string.Join(", ", result.Errors));
+                return View("Opetaja/Create", teacher);
+            }
+
+            UserManager.AddToRole(newUser.Id, "Opetaja");
+
+            teacher.ApplicationUserId = newUser.Id;
+            db.Teachers.Add(teacher);
+            db.SaveChanges();
+
+            return RedirectToAction(nameof(Teachers));
+        }
+
+        public ActionResult EditTeacher(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var teacher = db.Teachers.Find(id);
+            if (teacher == null) return HttpNotFound();
+
+            return View("Opetaja/Edit", teacher);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditTeacher(Teacher teacher, string NewPassword)
+        {
+            if (string.IsNullOrWhiteSpace(teacher.Email))
+                ModelState.AddModelError("Email", "Email is required");
+
+            if (!ModelState.IsValid)
+                return View("Opetaja/Edit", teacher);
+
+            var dbTeacher = db.Teachers.Find(teacher.Id);
+            if (dbTeacher == null) return HttpNotFound();
+
+            dbTeacher.Nimi = teacher.Nimi;
+            dbTeacher.Kvalifikatsioon = teacher.Kvalifikatsioon;
+            dbTeacher.Email = teacher.Email;
+
+            if (!string.IsNullOrEmpty(dbTeacher.ApplicationUserId))
+            {
+                var user = db.Users.Find(dbTeacher.ApplicationUserId);
+                if (user != null)
+                {
+                    user.Email = teacher.Email;
+                    user.UserName = teacher.Email;
+
+                    if (!string.IsNullOrWhiteSpace(NewPassword))
+                    {
+                        var token = UserManager.GeneratePasswordResetToken(user.Id);
+                        var passResult = UserManager.ResetPassword(user.Id, token, NewPassword);
+
+                        if (!passResult.Succeeded)
+                        {
+                            ModelState.AddModelError("", string.Join(", ", passResult.Errors));
+                            return View("Opetaja/Edit", teacher);
+                        }
+                    }
+                }
+            }
+
+            db.SaveChanges();
+            return RedirectToAction(nameof(Teachers));
+        }
+
+        public ActionResult DeleteTeacher(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var teacher = db.Teachers.Include(t => t.User).FirstOrDefault(t => t.Id == id);
+            if (teacher == null) return HttpNotFound();
+
+            return View("Opetaja/Delete", teacher);
+        }
+
+        [HttpPost, ActionName("DeleteTeacher")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteTeacherConfirmed(int id)
+        {
+            var teacher = db.Teachers.Find(id);
+            if (teacher == null) return HttpNotFound();
+
+            var trainings = db.Trainings.Where(t => t.OpetajaId == teacher.Id).ToList();
+            if (trainings.Any())
+                db.Trainings.RemoveRange(trainings);
+
+            if (!string.IsNullOrEmpty(teacher.ApplicationUserId))
+            {
+                var user = db.Users.Find(teacher.ApplicationUserId);
+                if (user != null)
+                {
+                    var roles = UserManager.GetRoles(user.Id).ToList();
+                    foreach (var r in roles)
+                        UserManager.RemoveFromRole(user.Id, r);
+
+                    UserManager.Delete(user);
+                }
+            }
+
+            db.Teachers.Remove(teacher);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Teachers));
+        }
+
+        // ---------------- TRAININGS ----------------
+        public ActionResult Trainings()
+        {
+            var trainings = db.Trainings
+                .Include(t => t.Keelekursus)
+                .Include(t => t.Opetaja)
+                .ToList();
+
+            return View("Trainings/Index", trainings);
+        }
+
+        public ActionResult CreateTraining()
+        {
+            ViewBag.Courses = new SelectList(db.Courses, "Id", "Nimetus");
+            ViewBag.Teachers = new SelectList(db.Teachers, "Id", "Nimi");
+            return View("Trainings/Create");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateTraining(Training training)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Courses = new SelectList(db.Courses, "Id", "Nimetus", training.KeelekursusId);
+                ViewBag.Teachers = new SelectList(db.Teachers, "Id", "Nimi", training.OpetajaId);
+                return View("Trainings/Create", training);
+            }
+
+            db.Trainings.Add(training);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Trainings));
+        }
+
+        public ActionResult EditTraining(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var training = db.Trainings.Find(id);
+            if (training == null) return HttpNotFound();
+
+            ViewBag.Courses = new SelectList(db.Courses, "Id", "Nimetus", training.KeelekursusId);
+            ViewBag.Teachers = new SelectList(db.Teachers, "Id", "Nimi", training.OpetajaId);
+            return View("Trainings/Edit", training);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditTraining(Training training)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Courses = new SelectList(db.Courses, "Id", "Nimetus", training.KeelekursusId);
+                ViewBag.Teachers = new SelectList(db.Teachers, "Id", "Nimi", training.OpetajaId);
+                return View("Trainings/Edit", training);
+            }
+
+            db.Entry(training).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction(nameof(Trainings));
+        }
+
+        public ActionResult DeleteTraining(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var training = db.Trainings
+                .Include(t => t.Keelekursus)
+                .Include(t => t.Opetaja)
+                .FirstOrDefault(t => t.Id == id);
+
+            if (training == null) return HttpNotFound();
+
+            return View("Trainings/Delete", training);
+        }
+
+        [HttpPost, ActionName("DeleteTraining")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteTrainingConfirmed(int id)
+        {
+            var training = db.Trainings.Find(id);
+            if (training == null) return HttpNotFound();
+
+            db.Trainings.Remove(training);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Trainings));
+        }
+
+        // ---------------- REGISTRATIONS (STAATUS) ----------------
+        public ActionResult Registrations()
+        {
+            var regs = db.Registrations
+                .Include(r => r.User)
+                .Include(r => r.Koolitus.Keelekursus)
+                .Include(r => r.Koolitus.Opetaja)
+                .OrderByDescending(r => r.Id)
+                .ToList();
+
+            return View("~/Views/Admin/Registrations/Index.cshtml", regs);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ConfirmRegistration(int id)
+        {
+            var reg = db.Registrations.Find(id);
+            if (reg == null) return HttpNotFound();
+
+            reg.Staatus = "Kinnitatud";
+            db.SaveChanges();
+
+            return RedirectToAction(nameof(Registrations));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CancelRegistration(int id)
+        {
+            var reg = db.Registrations.Find(id);
+            if (reg == null) return HttpNotFound();
+
+            reg.Staatus = "Tühistatud";
+            db.SaveChanges();
+
+            return RedirectToAction(nameof(Registrations));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ KeelteKooli/Migrations/Configuration.cs
@@ -0,0 +1,105 @@
+﻿using KeelteKooli.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace KeelteKooli.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<KeelteKooli.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(KeelteKooli.Models.ApplicationDbContext context)
+        {
+            // ---------------- ROLES ----------------
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            string[] roles = { "Admin", "Opetaja", "Opilane" };
+
+            foreach (var roleName in roles)
+            {
+                if (!roleManager.RoleExists(roleName))
+                {
+                    roleManager.Create(new IdentityRole(roleName));
+                }
+            }
+
+            // ---------------- ADMIN ----------------
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            string adminEmail = "admin@gmail.com";
+            var adminUser = userManager.FindByEmail(adminEmail);
+
+            if (adminUser == null)
+            {
+                adminUser = new ApplicationUser
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail
+                };
+                userManager.Create(adminUser, "Admin123!");
+                userManager.AddToRole(adminUser.Id, "Admin");
+            }
+
+            // ---------------- TEACHER ----------------
+            string teacherEmail = "teacher@gmail.com";
+            var teacherUser = userManager.FindByEmail(teacherEmail);
+
+            if (teacherUser == null)
+            {
+                teacherUser = new ApplicationUser
+                {
+                    UserName = teacherEmail,
+                    Email = teacherEmail
+                };
+                userManager.Create(teacherUser, "Teacher123!");
+                userManager.AddToRole(teacherUser.Id, "Opetaja");
+            }
+
+            if (!context.Teachers.Any(t => t.ApplicationUserId == teacherUser.Id))
+            {
+                context.Teachers.Add(new Teacher
+                {
+                    Nimi = "Irina Merkulova",
+                    Kvalifikatsioon = "English C1",
+                    FotoPath = "teacher1.jpg",
+                    ApplicationUserId = teacherUser.Id
+                });
+            }
+
+            // ---------------- COURSES ----------------
+            if (!context.Courses.Any())
+            {
+                var course = new Course
+                {
+                    Nimetus = "Английский для начинающих",
+                    Keel = "English",
+                    Tase = "A1",
+                    Kirjeldus = "Базовый курс английского языка"
+                };
+                context.Courses.Add(course);
+                context.SaveChanges();
+
+                var teacher = context.Teachers.FirstOrDefault();
+                if (teacher != null)
+                {
+                    context.Trainings.Add(new Training
+                    {
+                        KeelekursusId = course.Id,
+                        OpetajaId = teacher.Id,
+                        AlgusKuupaev = DateTime.Today,
+                        LoppKuupaev = DateTime.Today.AddMonths(1),
+                        Hind = 150,
+                        MaxOsalejaid = 10
+                    });
+                }
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
